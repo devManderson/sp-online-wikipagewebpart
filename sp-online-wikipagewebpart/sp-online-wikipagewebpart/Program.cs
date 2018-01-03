0a6eacd0c8aefc5e4e864d1cc1543a5827546934
Marco Anderson <dev.marco.anderson@gmail.com>
2018-01-03 11:59:04 +0100

    added a console project

---
+++ sp-online-wikipagewebpart/sp-online-wikipagewebpart/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sp_online_wikipagewebpart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
